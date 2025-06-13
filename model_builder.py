c0f422dccaa1412cd3f1bdfab3138ca1ee39ccf4
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-06-13 16:43:31 +0300

    Update model_builder.py

--- model_builder.py
+++ model_builder.py
@@ -6,25 +6,21 @@ from keras.regularizers import l2
 
 def build_model(input_shape, num_classes):
     model = Sequential([
-        # Blok 1
         Conv2D(32, (3,3), activation='relu', padding='same', input_shape=input_shape),
         BatchNormalization(),
         MaxPooling2D((2,2)),
         Dropout(0.2),
         
-        # Blok 2
         Conv2D(64, (3,3), activation='relu', padding='same'),
         BatchNormalization(),
         MaxPooling2D((2,2)),
         Dropout(0.3),
         
-        # Blok 3
         Conv2D(128, (3,3), activation='relu', padding='same'),
         BatchNormalization(),
         MaxPooling2D((2,2)),
         Dropout(0.4),
         
-        # Çıkış Katmanları
         Flatten(),
         Dense(256, activation='relu', kernel_regularizer=l2(0.001)),
         BatchNormalization(),
